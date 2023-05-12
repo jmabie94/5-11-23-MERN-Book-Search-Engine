c077848f1a7efe78e16f09e839d82bfc072cf9b7
Jack Mabie <johnmabie94@gmail.com>
2023-05-12 01:08:10 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 console.log(process.env.MONGODB_URI);
 
+// preparing for heroku deployment
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks');
 
 module.exports = mongoose.connection;
